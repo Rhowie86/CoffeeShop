4d5eaa0978c06a04d4a306bac165d7b49032053e
Rhowie86 <ronaldnhowie@gmail.com>
2021-04-07 16:00:01 -0500

    initial commit

---
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        private readonly string _connectionString;
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlConnection Connection
+        {
+            get { return new SqlConnection(_connectionString); }
+        }
+
+        public List<Coffee> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id, c.Title, c.BeanVarietyId b.Id b.[Name], b.Region, b.Notes FROM Coffee
+                                       JOIN BeanVariety b.Id ON; ";
+                    var reader = cmd.ExecuteReader();
+                    var coffees = new List<Coffee>();
+                    while (reader.Read())
+                    {
+                        var coffee = new Coffee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Name")),
+                            BeanVariety = new BeanVariety
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Region = reader.GetString(reader.GetOrdinal("Region")),
+
+                            },
+                        };
+
+                        coffee.BeanVarietyId = coffee.BeanVariety.Id;
+
+                        if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                        {
+                            coffee.BeanVariety.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                        }
+                        coffees.Add(coffee);
+                    }
+
+                    reader.Close();
+
+                    return coffees;
+                }
+            }
+        }
+    }
+}
+
